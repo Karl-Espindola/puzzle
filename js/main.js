6e63fb7f31d6f1a34ba2b69e164fd062f50d15d7
Karl <karlespindola2022@gmail.com>
2022-10-05 16:25:07 -0500

    manejo del DOM

---
+++ js/main.js
@@ -0,0 +1,106 @@
+let array_img=[];
+let fichas_ordenadas=[0,1,2,3,4,5,6,7,8];
+let fichas=[0,1,2,3,4,5,6,7,8];
+let num_click=0;
+let casilla=0;
+let contador=0;
+let src_img_a="";
+let src_img_b="";
+let img_a;
+let posicion_1;
+
+let num_img;
+
+
+function mover(){
+    for(let i=0; i<9; i++){
+     document.getElementById("img"+(i+1)).src="img/"+(fichas[i]+1)+".jpg";
+
+     document.getElementById("img"+(i+1)).style="border:none"
+    }
+}
+function seleccionar(casilla){
+ 
+    num_click++;
+    
+    if(num_click==1){
+       
+        posicion_1=casilla;
+        console.log(casilla);
+        
+    }
+    else if(num_click==2){
+        let posicion_2=casilla;
+        let intercambio= fichas[posicion_1]
+        fichas[posicion_1]=fichas[posicion_2]
+        fichas[posicion_2]=intercambio;
+        
+        
+        console.log(fichas);
+        mover();
+        document.getElementById("img"+(casilla+1)).style.border="4px solid orange"
+        
+        num_click=0;
+    }
+    
+
+}
+function verificar(){
+
+    for(let i=0; i<array_img.length; i++){
+        console.log(contador);
+        
+        if(fichas[i]!=fichas_ordenadas[i]){
+            contador++;
+            continue
+        }
+    }
+    if(contador!=0){
+        alert("Desordenado");
+    }
+    else{
+        alert("ordenado");
+    }
+    contador=0;
+}
+
+
+function desordenar(){
+    fichas=fichas.sort(function(){
+        return Math.random()-0.5;
+    })
+    console.log(fichas);
+    mover();
+}
+function verificar(){
+
+    for(let i=0; i<9; i++){
+        console.log(contador);
+        
+        if(fichas[i]!=fichas_ordenadas[i]){
+            contador++;
+            continue
+        }
+    }
+    if(contador!=0){
+        alert("Desordenado");
+    }
+    else{
+        alert("ordenado");
+    }
+    contador=0;
+}
+        
+let btn_veri=document.getElementById("btnVerificar");
+btn_veri.addEventListener("click",verificar);
+
+
+
+desordenar()
+
+
+
+
+
+
+
